27851453b575a68829159be262bc4a227d4c84c7
Elizeu Barbosa Abreu <elizeubarbosaabreu@yahoo.com>
2023-12-14 18:30:32 -0400

    app add

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
 import streamlit as st
-import pandas as pd
+import requests
 
-st.write(
-    '''
-    # Meu primeiro app com streamlit
-    Este é minha primeira aplicação web utilizando streamlit.
+def buscar_cep(cep):
+    url = f"https://viacep.com.br/ws/{cep}/json"
+    response = requests.get(url)
 
-    '''
-)
+    if response.status_code == 200: 
+        data = response.json()
+        return data
+    
+    else:
+        return None
+
+st.title("Buscador de CEP")
+st.write('---')
+
+cep_input = st.text_input("Digite o CEP desejado:")
+
+if st.button("Buscar"):
+    if cep_input:
+        endereco = buscar_cep(cep_input)
+        if endereco:
+            st.write(f"CEP: {endereco['cep']}")
+            st.write(f"LOGRADOURO: {endereco['logradouro']}")
+            st.write(f"COMPLEMENTO: {endereco['complemento']}")
+            st.write(f"BAIRO: {endereco['bairro']}")
+            st.write(f"CIDADE: {endereco['localidade']}")
+            st.write(f"ESTADO: {endereco['uf']}")
+        else:
+            st.write('Verifique o CEP digitado...')
+    else:
+        st.write('Verifique o CEP digitado ou sua conexão à internet...')
+
+st.write('''
+        ---
+         Exemplo de CEP: 76916-000
+         
+         Aplicação desenvolvida por [Elizeu Barbosa Abreu](https://github.com/elizeubarbosaabreu)''')
+        
