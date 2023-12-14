d6cace81d19d318de2696287e9869fd5590ca72c
Elizeu Barbosa Abreu <elizeubarbosaabreu@yahoo.com>
2023-12-14 18:35:26 -0400

    add exceções

--- app.py
+++ app.py
@@ -17,24 +17,27 @@ st.write('---')
 
 cep_input = st.text_input("Digite o CEP desejado:")
 
-if st.button("Buscar"):
-    if cep_input:
-        endereco = buscar_cep(cep_input)
-        if endereco:
-            st.write(f"CEP: {endereco['cep']}")
-            st.write(f"LOGRADOURO: {endereco['logradouro']}")
-            st.write(f"COMPLEMENTO: {endereco['complemento']}")
-            st.write(f"BAIRO: {endereco['bairro']}")
-            st.write(f"CIDADE: {endereco['localidade']}")
-            st.write(f"ESTADO: {endereco['uf']}")
+try:
+    if st.button("Buscar"):
+        if cep_input:
+            endereco = buscar_cep(cep_input)
+            if endereco:
+                st.write(f"CEP: {endereco['cep']}")
+                st.write(f"LOGRADOURO: {endereco['logradouro']}")
+                st.write(f"COMPLEMENTO: {endereco['complemento']}")
+                st.write(f"BAIRO: {endereco['bairro']}")
+                st.write(f"CIDADE: {endereco['localidade']}")
+                st.write(f"ESTADO: {endereco['uf']}")
+            else:
+                st.write('Verifique o CEP digitado...')
         else:
-            st.write('Verifique o CEP digitado...')
-    else:
-        st.write('Verifique o CEP digitado ou sua conexão à internet...')
+            st.write('Verifique o CEP digitado ou sua conexão à internet...')
+except:
+    st.write('putz')
 
 st.write('''
         ---
          Exemplo de CEP: 76916-000
-         
+
          Aplicação desenvolvida por [Elizeu Barbosa Abreu](https://github.com/elizeubarbosaabreu)''')
         
